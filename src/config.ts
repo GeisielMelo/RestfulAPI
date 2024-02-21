6545b522bdc29e6b9438901ded8abcf44d9a3ebb
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-21 14:50:52 -0300

    raw: removed redis; add mongo

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  nodeEnv:
-    (process.env.NODE_ENV as 'development' | 'production') || 'production',
+  nodeEnv: (process.env.NODE_ENV as 'development' | 'production') || 'production',
   port: Number(process.env.PORT) || 3000,
   database: {
     url: process.env.DATABASE_URL,
   },
-  redis: {
-    url: process.env.REDIS_URL,
+  mongo: {
+    uri: process.env.MONGODB_URI,
   },
   auth: {
     secret: process.env.AUTH_SECRET || 'dev',
