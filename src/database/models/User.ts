dbd13d81de770184c28337d86db82c7778ca449a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-26 17:56:20 -0300

    feat: Updated user model to handle with new authentication method.

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -3,6 +3,12 @@ import { Schema, Document, model } from 'mongoose'
 interface IUser {
   email: string
   password: string
+  roles: {
+    User: number
+    Editor: number
+    Admin: number
+  }
+  refreshToken: string[]
 }
 
 interface IUserDocument extends IUser, Document {}
@@ -11,6 +17,12 @@ const userSchema = new Schema<IUserDocument>(
   {
     email: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
+    refreshToken: { type: [String] },
+    roles: {
+      User: { type: Number, default: 2001 },
+      Editor: { type: Number },
+      Admin: { type: Number },
+    },
   },
   {
     timestamps: true,
