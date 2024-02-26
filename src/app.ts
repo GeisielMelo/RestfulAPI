93edcc0eb3868eb1dfa76998184e2e8587d62652
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-26 18:01:18 -0300

    feat: add exception handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import routes from './routes'
+import corsOptions from './config/cors'
 
 export default class App {
   app: Application
@@ -21,13 +23,19 @@ export default class App {
 
   private middlewares() {
     this.app.use(express.json())
-    this.app.use(express.urlencoded({ extended: true }))
-    this.app.use(cors())
+    this.app.use(express.urlencoded({ extended: false }))
+    this.app.use(cors(corsOptions))
+    this.app.use(cookieParser())
   }
 
   private routes() {
     this.app.use(routes)
   }
 
-  private exceptionHandler() {}
+  private exceptionHandler() {
+    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error(err.message)
+      next()
+    })
+  }
 }
