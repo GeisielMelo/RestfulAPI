8f94b509ddfd5a0b0ea2cbb4cdde3075c186e9d2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-02 12:38:08 -0300

    fix: fix TokenExpired functions

--- src/app/Auth/services/AuthService.ts
+++ src/app/Auth/services/AuthService.ts
@@ -1,78 +1,63 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt, { JsonWebTokenError, JwtPayload } from 'jsonwebtoken'
 import config from '../../../config'
-import database from '../../../database'
 import AuthError from '../exceptions/AuthError'
-import User from '../../../database/models/User'
 
-interface ITokens {
-  accessToken: string
-  refreshToken: string
-}
-interface IRoles {
-  User: number
-  Editor: number
-  Admin: number
-}
-
-export default class AuthService {
-  async generateTokens(id: string, roles: IRoles): Promise<ITokens> {
+class AuthService {
+  async createAccessToken(id: string): Promise<string> {
     try {
-      const accessToken = jwt.sign({ id, roles }, config.auth.secret.access, {
+      return jwt.sign({ id }, config.auth.secret.access, {
         expiresIn: config.auth.expiresIn.access,
       })
-
-      const refreshToken = jwt.sign({ id }, config.auth.secret.refresh, {
-        expiresIn: config.auth.expiresIn.refresh,
-      })
-
-      return { accessToken, refreshToken }
     } catch (error) {
-      throw new AuthError('Error on generateTokens.')
+      throw new AuthError('Error on access token creation.')
     }
   }
 
-  async validateAccessToken(token: string): Promise<string | JwtPayload> {
+  async createRefreshToken(id: string): Promise<string> {
+    try {
+      return jwt.sign({ id }, config.auth.secret.refresh, {
+        expiresIn: config.auth.expiresIn.refresh,
+      })
+    } catch (error) {
+      throw new AuthError('Error on refresh token creation.')
+    }
+  }
+
+  async verifyAccessToken(token: string): Promise<string | JwtPayload> {
     return jwt.verify(token, config.auth.secret.access) as JwtPayload
   }
 
-  async validateRefreshToken(refreshToken: string): Promise<string | JwtPayload> {
-    try {
-      await database.connect()
-      const user = await User.findOne({ refreshToken })
-
-      if (!user) throw new AuthError('User not found.')
-
-      if (!user.refreshToken.length) throw new AuthError('Token not found.')
-
-      user.refreshToken.forEach((element) => {
-        if (element === refreshToken) return
-        throw new AuthError("Token doesn't exists.")
-      })
-
-      return jwt.verify(refreshToken, config.auth.secret.refresh)
-    } catch (error) {
-      if (error instanceof AuthError) throw new AuthError(error.message)
-      throw new AuthError('Error on validateRefreshToken.')
-    } finally {
-      await database.disconnect()
-    }
+  async verifyRefreshToken(refreshToken: string): Promise<string | JwtPayload> {
+    return jwt.verify(refreshToken, config.auth.secret.refresh)
   }
 
   async isRefreshTokenExpired(token: string): Promise<boolean> {
     try {
-      const decoded = jwt.verify(token, config.auth.secret.refresh) as JwtPayload
-      return decoded.exp ? Date.now() >= new Date(decoded.exp * 1000).getTime() : false
-    } catch {
+      this.verifyRefreshToken(token)
+      return false
+    } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        if (error.name === 'TokenExpiredError') {
+          return true
+        }
+      }
       return true
     }
   }
 
   async isAccessTokenExpired(token: string): Promise<boolean> {
     try {
-      const decoded = jwt.verify(token, config.auth.secret.refresh) as JwtPayload
-      return decoded.exp ? Date.now() >= new Date(decoded.exp * 1000).getTime() : false
-    } catch {
+      this.verifyAccessToken(token)
+      return false
+    } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        if (error.name === 'TokenExpiredError') {
+          return true
+        }
+      }
       return true
     }
   }
 }
+
+export default new AuthService()
