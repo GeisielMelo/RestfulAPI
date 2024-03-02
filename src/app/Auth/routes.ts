7b732d84a8c35d775af9fa3c704ae5925d903ffd
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-02 12:36:52 -0300

    feat: Updated routes and added middlewares.

--- src/app/Auth/routes.ts
+++ src/app/Auth/routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import auth from './controllers/AuthController'
-import AuthMiddleware from './middlewares/AuthMiddleware'
+import AuthMiddleware from '../../middlewares/AuthMiddleware'
+import RefreshTokenValidator from './validators/RefreshTokenValidator'
 
 const routes = Router()
 
 routes.post('/api/auth/sign-in', auth.create)
 routes.delete('/api/auth/sign-out', auth.delete)
-routes.get('/api/auth/sign-refresh', auth.update)
+
+routes.get('/api/auth/jwt', RefreshTokenValidator, auth.update)
 
 // TODO Remove this function.
-routes.get('/api/auth/test', AuthMiddleware, auth.teste)
+routes.get('/api/auth/test', AuthMiddleware, auth.test)
 
 export default routes
