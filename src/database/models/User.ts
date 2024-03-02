ce8832550447b51688bd011a2d99a0cef886b24a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-02 12:33:53 -0300

    feat: export interface IUserDocument

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -3,26 +3,16 @@ import { Schema, Document, model } from 'mongoose'
 interface IUser {
   email: string
   password: string
-  roles: {
-    User: number
-    Editor: number
-    Admin: number
-  }
   refreshToken: string[]
 }
 
-interface IUserDocument extends IUser, Document {}
+export interface IUserDocument extends IUser, Document {}
 
 const userSchema = new Schema<IUserDocument>(
   {
     email: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     refreshToken: { type: [String] },
-    roles: {
-      User: { type: Number, default: 2001 },
-      Editor: { type: Number },
-      Admin: { type: Number },
-    },
   },
   {
     timestamps: true,
