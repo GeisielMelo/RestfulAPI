b18a82e544b834577cbcbbd072102d8c3f084910
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-02 12:34:54 -0300

    feat: Added ACCESS_OPTIONS and updated maxAge.

--- src/config/cookies/index.ts
+++ src/config/cookies/index.ts
@@ -1,15 +1,23 @@
 import { CookieOptions } from 'express'
 
-/** Default configuration to create a secure cookie for 30 days. */
-export const COOKIE_SET_OPTIONS: CookieOptions = {
+/** Default configuration to create a secure cookie for 7 days. */
+export const REFRESH_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: true,
   // sameSite: 'none',
-  maxAge: 30 * 24 * 60 * 60 * 1000,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+}
+
+/** Default configuration to create a secure cookie for 6 horas. */
+export const ACCESS_OPTIONS: CookieOptions = {
+  httpOnly: true,
+  secure: true,
+  // sameSite: 'none',
+  maxAge: 6 * 60 * 60 * 1000,
 }
 
 /** Default configuration to delete cookie data. */
-export const COOKIE_UNSET_OPTIONS: CookieOptions = {
+export const UNSET_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: 'none',
